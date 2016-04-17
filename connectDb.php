5bf565d3d7634ae04042382587c747469b53d7ab
Amy Tian <Amy Tian>
2016-04-17 17:15:26 -0400

    .

--- connectDb.php
+++ connectDb.php
@@ -3,7 +3,7 @@
        // Connect to the database for the entry of the CSV stuff into the database.
       $dbhost = 'localhost';
       $dbuser = 'atian_atian';     // CHANGE IT TO YOUR DATABASE USER NAME
-      $dbpass = 'ORF401';            // CHANGE IT TO YOUR DATABASE PASSWORD
+      $dbpass = 'orf401';            // CHANGE IT TO YOUR DATABASE PASSWORD
       $dbname = 'atian_bs';     // CHANGE IT TO YOUR DATABASE NAME
       
       $conn = mysql_connect($dbhost, $dbuser, $dbpass) or die('Error connecting to mysql');

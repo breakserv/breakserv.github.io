6f29d5565a4068ef124dd282966c41cc5ff4e248
Amy Tian <Amy Tian>
2016-04-17 22:03:13 -0400

    .

--- login.php
+++ login.php
@@ -7,7 +7,7 @@
       echo "<html>";
       echo "<title> Empty fields </title>";
       echo '<STYLE TYPE="text/css">BODY { font-family:sans-serif;}</STYLE>';
-      echo '<BODY BGCOLOR="#c5d9c1" TEXT = "black">';
+      echo '<BODY BGCOLOR="#313131" TEXT = "white">';
       echo "Empty Field. Please try again. Redirecting you back. ";
       echo '<META HTTP-EQUIV="REFRESH" CONTENT="1; URL=index.html">';
       echo '</body>';
@@ -21,7 +21,7 @@
           echo "<html>";
           echo' <title> User does not exist </title>';
           echo '<STYLE TYPE="text/css">BODY { font-family:sans-serif;}</STYLE>';
-          echo '<BODY BGCOLOR="#c5d9c1" TEXT = "black">';
+          echo '<BODY BGCOLOR="#313131" TEXT = "white">';
           echo ' User not found. Please try again. Redirecting you back. ';
           echo '<META HTTP-EQUIV="REFRESH" CONTENT="3; URL=index.html">';
           echo '</body>';
@@ -32,240 +32,160 @@
                 echo "<html>";
                 echo ' <title> Incorrect Password </title>';
                 echo '<STYLE TYPE="text/css">BODY { font-family:sans-serif;}</STYLE>';
-                echo '<BODY BGCOLOR="#c5d9c1" TEXT = "black">';
+                echo '<BODY BGCOLOR="#313131" TEXT = "white">';
                 echo "Wrong password. Please try again. Redirecting you back. ";
                 echo '<META HTTP-EQUIV="REFRESH" CONTENT="3; URL=index.html">';
                 echo '</body>';
                 echo ' </html>';
 
              } else {
-              echo "<html>";
-              echo " <title> Pear - " .$row[fName]. "'s profile. </title>";
-              echo '<STYLE TYPE="text/css">BODY { font-family:sans-serif;}</STYLE>';
-              echo '<BODY BGCOLOR="#c5d9c1" TEXT = "black">';
-              echo '<script type ="text/javascript" src="isEmail.js"></script>
-<script type="text/javascript" src="isBlank.js"></script>
-<script type ="text/javascript" src="checkTrip.js"></script>';
-              echo '<center> <IMG src="Logo.png" width="10%" height="auto"> <br><br>' ;
-              echo '<b>Hi, ' .$row[fName]. '. <i>Welcome back!</i> </b><br>';
 
-              echo '<SCRIPT TYPE="text/javascript"> 
-              function popup(url) {
-                  if (! window.focus)return true; 
-                  var href; 
-                  if (typeof(url) == "string") href=url; 
-                  else href=url.href; 
-                  window.open(href, "popupWindow", "width=500,height=700,scrollbars=yes"); 
-                  return false; 
-              } 
-              </SCRIPT>';
 
-              echo '<h3>Your Information</h3>';
-              echo "<form name='editU' action='editUser.php' method='post'>";
-              echo "<input type='hidden' name='Pass' value=$pass />";
-              echo "<input type='hidden' name='User' value=$user />";
-              echo '<CENTER><TABLE BGCOLOR="#c5d9c1" BORDER=0 WIDTH=25%>';
-              echo '<tr><td><b>First Name:</b></td><td>'.$row[fName].'</td></tr>';
-              echo '<tr><td><b>Last Name:</b></td><td>'.$row[lName].'</td></tr>';
-              echo '</TABLE></CENTER>';
-              echo "<br><input type='submit' value='Update User Info'><br>";
-              echo "</form>";
-              echo '<br>';
+echo "<html>";
 
-              echo '<h3>Your Current Trips</h3>';
-              echo "<CENTER> <TABLE BGCOLOR=white BORDER=1 CELLSPACING=2 CELLPADDING=4 WIDTH=60%>";
-              echo "<TR BGCOLOR=white>";
-              echo "<TH>Trip ID</TH> ";
-              echo "<TH>Origin City</TH>";
-              echo "<TH>Origin State</TH>";
-              echo "<TH>Destination City</TH>";
-              echo "<TH>Destination State</TH>";
-              echo "<TH>Departure Date</TH>";
-              echo "<TH>Departure Time</TH>";
-              echo "<TH>Has Car</TH>";
-              echo "<TH>Available Seats</TH>";
-              echo "</TR>";
+// ***********************************************************************
+// SKELETAL CODE FROM http://startbootstrap.com/template-overviews/agency/ 
+// ***********************************************************************
 
-             /* include ("connectDb.php");
-              $sqlt = "SELECT * FROM ridersdb INNER JOIN tripdb ON emailtrip = emailriders WHERE emailriders = '$email' ";
+echo "<head>";
+    
+    echo "<meta charset='utf-8'>";
+    echo "<meta http-equiv='X-UA-Compatible' content='IE=edge'>";
+    echo "<meta name='viewport' content='width=device-width, initial-scale=1'>";
+    echo '<meta name="description" content="">';
+    echo '<meta name="author" content="">';
 
-              // Again, Send the request
-              $result = mysql_query($sqlt);
+    echo " <title> BreakServ - " .$row[fName]. "'s profile. </title>";
+    echo '<BODY BGCOLOR="#313131" TEXT = "white">';
 
-              while($r2 = mysql_fetch_array($result)) {
-                  echo "<TR>";
-                  echo "<TD ALIGN=CENTER> ".$r2["tripID"]." </TD>";
-                  echo "<TD ALIGN=CENTER> ".$r2["oCity"]." </TD>";
-                  echo "<TD ALIGN=CENTER> ".$r2["oState"]." </TD>";
-                  echo "<TD ALIGN=CENTER> ".$r2["dCity"]." </TD>";
-                  echo "<TD ALIGN=CENTER> ".$r2["dState"]." </TD>";
-                  echo "<TD ALIGN=CENTER> ".$r2["dDate"]." </TD>";
-                  echo "<TD ALIGN=CENTER> ".$r2["dTime"]." </TD>";
-                  echo "<TD ALIGN=CENTER> ".$r2["Hascar"]." </TD>";
-                  echo "<TD ALIGN=CENTER> ".$r2["Seats"]." </TD>";
-                  echo "</TR>";
-                }
+    echo '<link href="css/bootstrap.min.css" rel="stylesheet">';
+    echo '<link href="css/agency.css" rel="stylesheet">';
+    echo '<link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">';
+    echo '<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">';
+    echo "<link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>";
+    echo "<link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>";
+    echo "<link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>";
 
-              mysql_close($conn);
-                 
-              echo "</TABLE></CENTER>"; */
-              echo "<br>";
+    echo "<!--[if lt IE 9]>";
+        echo '<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script><script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>';
+    echo "<![endif]-->";
 
-              echo "<H3>Update or Add a Trip</H3>";
-              echo "<form name='new' action='updateTrip.php' method='post' onsubmit='return checkTrip();'>";
-              echo '<i>Leave "Trip ID" field BLANK to add a new trip</i>';
-              echo '<table width = 400><tr><td>Trip ID: </td><td><input type="text" name="tripID" /></td></tr><tr></tr><tr><td>Origin City:</td> <td> <input type="text" name="oCity" /> </td><td id="oCityReq"></td></tr>
-<tr><td>Origin State:</td> <td> <select name="oState">
-  <option selected disabled value="">-Select State-</option>
-  <option value="AL">Alabama</option>
-  <option value="AK">Alaska</option>
-  <option value="AZ">Arizona</option>
-  <option value="AR">Arkansas</option>
-  <option value="CA">California</option>
-  <option value="CO">Colorado</option>
-  <option value="CT">Connecticut</option>
-  <option value="DE">Delaware</option>
-  <option value="DC">District Of Columbia</option>
-  <option value="FL">Florida</option>
-  <option value="GA">Georgia</option>
-  <option value="HI">Hawaii</option>
-  <option value="ID">Idaho</option>
-  <option value="IL">Illinois</option>
-  <option value="IN">Indiana</option>
-  <option value="IA">Iowa</option>
-  <option value="KS">Kansas</option>
-  <option value="KY">Kentucky</option>
-  <option value="LA">Louisiana</option>
-  <option value="ME">Maine</option>
-  <option value="MD">Maryland</option>
-  <option value="MA">Massachusetts</option>
-  <option value="MI">Michigan</option>
-  <option value="MN">Minnesota</option>
-  <option value="MS">Mississippi</option>
-  <option value="MO">Missouri</option>
-  <option value="MT">Montana</option>
-  <option value="NE">Nebraska</option>
-  <option value="NV">Nevada</option>
-  <option value="NH">New Hampshire</option>
-  <option value="NJ">New Jersey</option>
-  <option value="NM">New Mexico</option>
-  <option value="NY">New York</option>
-  <option value="NC">North Carolina</option>
-  <option value="ND">North Dakota</option>
-  <option value="OH">Ohio</option>
-  <option value="OK">Oklahoma</option>
-  <option value="OR">Oregon</option>
-  <option value="PA">Pennsylvania</option>
-  <option value="RI">Rhode Island</option>
-  <option value="SC">South Carolina</option>
-  <option value="SD">South Dakota</option>
-  <option value="TN">Tennessee</option>
-  <option value="TX">Texas</option>
-  <option value="UT">Utah</option>
-  <option value="VT">Vermont</option>
-  <option value="VA">Virginia</option>
-  <option value="WA">Washington</option>
-  <option value="WV">West Virginia</option>
-  <option value="WI">Wisconsin</option>
-  <option value="WY">Wyoming</option>
-</select></td><td id="oStateReq"></td></tr>
-<tr><td>Destination City: </td> <td> <input type="text" name="dCity" /> </td><td id="dCityReq"></td> </tr>
-<tr><td>Destination State:</td> <td> <select name="dState">
-  <option selected disabled value="">-Select State-</option>
-  <option value="AL">Alabama</option>
-  <option value="AK">Alaska</option>
-  <option value="AZ">Arizona</option>
-  <option value="AR">Arkansas</option>
-  <option value="CA">California</option>
-  <option value="CO">Colorado</option>
-  <option value="CT">Connecticut</option>
-  <option value="DE">Delaware</option>
-  <option value="DC">District Of Columbia</option>
-  <option value="FL">Florida</option>
-  <option value="GA">Georgia</option>
-  <option value="HI">Hawaii</option>
-  <option value="ID">Idaho</option>
-  <option value="IL">Illinois</option>
-  <option value="IN">Indiana</option>
-  <option value="IA">Iowa</option>
-  <option value="KS">Kansas</option>
-  <option value="KY">Kentucky</option>
-  <option value="LA">Louisiana</option>
-  <option value="ME">Maine</option>
-  <option value="MD">Maryland</option>
-  <option value="MA">Massachusetts</option>
-  <option value="MI">Michigan</option>
-  <option value="MN">Minnesota</option>
-  <option value="MS">Mississippi</option>
-  <option value="MO">Missouri</option>
-  <option value="MT">Montana</option>
-  <option value="NE">Nebraska</option>
-  <option value="NV">Nevada</option>
-  <option value="NH">New Hampshire</option>
-  <option value="NJ">New Jersey</option>
-  <option value="NM">New Mexico</option>
-  <option value="NY">New York</option>
-  <option value="NC">North Carolina</option>
-  <option value="ND">North Dakota</option>
-  <option value="OH">Ohio</option>
-  <option value="OK">Oklahoma</option>
-  <option value="OR">Oregon</option>
-  <option value="PA">Pennsylvania</option>
-  <option value="RI">Rhode Island</option>
-  <option value="SC">South Carolina</option>
-  <option value="SD">South Dakota</option>
-  <option value="TN">Tennessee</option>
-  <option value="TX">Texas</option>
-  <option value="UT">Utah</option>
-  <option value="VT">Vermont</option>
-  <option value="VA">Virginia</option>
-  <option value="WA">Washington</option>
-  <option value="WV">West Virginia</option>
-  <option value="WI">Wisconsin</option>
-  <option value="WY">Wyoming</option>
-</select></td><td id="dStateReq"></td></tr>
-<tr><td>Departure Date:</td> <td> <input type="date" name="dDate" value="1976-01-31" /> </td><td id="dDateReq"></td></tr>
-<tr><td>Departure Time: </td> <td> <input type="time" name="dTime" value="00:00:00" /> </td><td id="dTimeReq"></td> </tr>
-</table>
+echo "</head>";
 
-<br>Do you have a car? <br>
-<table width = 100>
-<tr><td> <input type="radio" name="Hascar" value="YES" />  </td> <td>Yes </td></tr>
-<tr><td> <input type="radio" name="Hascar" value="NO" /> </td> <td> No </td></tr>
-</table>
-<br>If you have a car, how many free seats do you have?<br>
-<table width = 100>
-<tr><td> <input type="number" name="Seats" value="0" />  </td> <td id="SeatsReq"></td></tr>
-</table>';
-              echo "<input type='hidden' name='Pass' value=$pass />";
-              echo "<input type='hidden' name='User' value=$user />";
-              echo "<br><input type='submit' value='Update'><br>";
-              echo "</form><br>";
+echo '<body id="page-top" class="index">
 
-              echo "<H3>Delete a Trip</H3>";
-              echo "<form name='new1' action='deleteTrip.php' method='post' onsubmit='return !isBlank(\"new1\", \"tripID1\");'>";
-              echo '<table width = 400><tr><td>Trip ID: </td><td><input type="text" name="tripID1" /></td></tr></table>';
-              echo "<input type='hidden' name='Pass' value=$pass />";
-              echo "<input type='hidden' name='User' value=$user />";
-              echo "<br><input type='submit' value='Delete'><br>";
-              echo "</form><br>";
+    <!-- Navigation -->
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand page-scroll" href="http://atian.mycpanel2.princeton.edu/breakserv/index.html">BreakServ</a>
+            </div>
 
-              echo '<H3>Log Out or Unsubscribe</H3>';
-              echo '<a href="index.html"><button>Log Out</button></a><br><br>';
-              echo '<form id="unsubscribe" action="deleteMember.php" method="post">';
-              echo "<input type='hidden' name='User' value=$user />";
-              echo "<input type='submit' value='Unsubscribe'><br>";
-              echo '</form>';
-              echo "<br>";
-              echo '<hr>';
-              echo ' &copy PEAR app';
-              echo "<br>";
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
 
-              echo ' <font size = 1> Ride share with <i>PEAR</i>! </font>';
-              echo '</center>';
-              echo "<br>";
-              echo "</body>";
-              echo ' </html>';
+    <!-- Main Page -->
+    <section id="homepage">
+    <div class="container">
+         <div class="row">
+             <div class="col-lg-12 text-center">
+                 <div class="intro-heading"><img src="http://atian.mycpanel2.princeton.edu/breakserv/img/BSLogo.png" width=80%></div>
+                 <h2 class="section-heading">Hi, ' .$row[fName]. '. Welcome back!</h2>';
+
+                 // CHECK IF USER IS NEW OR NOT. If new, display "get started here!" to start the authentication/scraping process.
+                 // If isNew = 1, basically.
+
+
+                 // else....display map with pins
+
+      // NOTE: THIS NEEDS TO BE CHANGED LATER TO ACTUAL EMAIL! (currently, only have username + pass info)
+     echo '<h3 class="section-subheading text-muted">Here are your events from ' .$row[User]. ' email.</h3> 
+             </div>
+         </div>
+    <center>
+         <!-- MAP DISPLAY GOES HERE!!!!!!!!!!!!!!!!!1 -->
+
+         <!-- Button to display all events and add, edit, or delete any one of them -->
+         <form action="http://google.com">
+            <button type="submit" class="btn btn-xl">View, Add, Delete, or Edit Your Events</button>
+         </form>
+
+
+         <BR><BR>
+         <form action="index.html">
+            <button type="submit" class="btn btn-xl">Logout</button>
+         </form>
+
+    </center>
+    </section>
+
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <span class="copyright">Copyright &copy; BreakServ 2016</span>
+                </div>
+                <div class="col-md-4">
+                    <ul class="list-inline social-buttons">
+                        <li><a href="#"><i class="fa fa-twitter"></i></a>
+                        </li>
+                        <li><a href="#"><i class="fa fa-facebook"></i></a>
+                        </li>
+                        <li><a href="#"><i class="fa fa-linkedin"></i></a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-md-4">
+                    <ul class="list-inline quicklinks">
+                        <li> <a href="index.html">Unsubscribe</a></h3> 
+                        </li>
+                        <li><a href="#">Privacy Policy</a>
+                        </li>
+                        <li><a href="#">Terms of Use</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+    <script src="js/classie.js"></script>
+    <script src="js/cbpAnimatedHeader.js"></script>
+
+    <!-- Contact Form JavaScript -->
+    <script src="js/jqBootstrapValidation.js"></script>
+    <script src="js/contact_me.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="js/agency.js"></script>
+
+</body>';
+echo '</html>';
            }
         }
    }

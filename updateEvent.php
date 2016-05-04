0e536a0bfc642c438abed9dc713298cfde3c54c6
Amy Tian <Amy Tian>
2016-05-04 18:20:10 -0400

    .

--- updateEvent.php
+++ updateEvent.php
@@ -20,13 +20,33 @@
       include ("readDb.php");
       include ("connectDb.php");
 
+      // Adding an event
       if (!$eID) {
-            echo "<br><br><br><br><br><center><h1><b><i>Adding event now...</h1></center>";
-            if ($isFood == 1) {
-                  $sql = "INSERT INTO Events (eID, eUser, eventName, startDate, eTime, eLocation, isFood, eDetails) VALUES ('$eID', '$user', '$eName', '$startDate', '$eTime', '$eLocation', '$isFood', '$eDetails')";
+            // Make sure free users are not over capacity
+            if ($row[isFree] == 1 && $row[CurrCount] == 5) {
+                  echo "<br><br><br><br><br><center><h1><b><i>You are over capacity (5 events). Please delete an event to proceed.<BR>You will be redirected momentarily.</h1></center>";
+                  sleep(2);
+                  echo '<form id="autologin" action="allevents.php" method="post">';
+                  echo "<input type='hidden' name='User' value=$user />";
+                  echo "<input type='hidden' name='Pass' value=$pass />";
+                  echo '</form>';
+                  echo '<script language="javascript">';
+                  echo 'document.getElementById("autologin").submit();';
+                  echo '</script>';
             }
             else {
-                  $sql = "INSERT INTO Events (eID, eUser, eventName, startDate, eTime, eLocation, isFood, eDetails) VALUES ('$eID', '$user', '$eName', '$startDate', '$eTime', '$eLocation', 0, '$eDetails')";
+                  // Update CurrCount
+                  $newCount = $row[CurrCount] + 1;
+                  $sql = "UPDATE Members SET CurrCount='$newCount' WHERE User='$user'";
+                  $result = mysql_query($sql);
+
+                  echo "<br><br><br><br><br><center><h1><b><i>Adding event now...</h1></center>";
+                  if ($isFood == 1) {
+                        $sql = "INSERT INTO Events (eID, eUser, eventName, startDate, eTime, eLocation, isFood, eDetails) VALUES ('$eID', '$user', '$eName', '$startDate', '$eTime', '$eLocation', '$isFood', '$eDetails')";
+                  }
+                  else {
+                        $sql = "INSERT INTO Events (eID, eUser, eventName, startDate, eTime, eLocation, isFood, eDetails) VALUES ('$eID', '$user', '$eName', '$startDate', '$eTime', '$eLocation', 0, '$eDetails')";
+                  }
             }
       } else {
             echo "<br><br><br><br><br><center><h1><b><i>Updating event now...</h1></center>";

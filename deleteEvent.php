bcd57240eddd87cf0972218bc2ed3bf64e7c54c8
Amy Tian <Amy Tian>
2016-05-04 19:18:23 -0400

    .

--- deleteEvent.php
+++ deleteEvent.php
@@ -25,6 +25,22 @@
             // Re-update the currCount to be 0
             $sql2 = "UPDATE Members SET CurrCount = 0 WHERE User='$user'";
             $result2 = mysql_query($sql2);
+
+            if ($result2==1){
+                  $newCount = $row[CurrCount] - 1;
+                  //echo '<BR>'.$newCount;
+                  $sql = "UPDATE Members SET CurrCount = '$newCount' WHERE User='$user'";
+                  $result = mysql_query($sql);
+                  sleep(2);
+                  echo '<form id="autologin" action="allevents.php" method="post">';
+                  echo "<input type='hidden' name='User' value=$user />";
+                  echo "<input type='hidden' name='Pass' value=$pass />";
+                  echo '</form>';
+                  echo '<script language="javascript">';
+                  echo 'document.getElementById("autologin").submit();';
+                  echo '</script>';
+            } else
+                      echo '<br><br><br><br><br><center><h1><font color="red"> <b><i> Error. Please Try Again. </font></h1></center>'; 
       }
 
       else {

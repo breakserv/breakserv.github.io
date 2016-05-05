69bac067bb1507930ccce28ce1f93657f0c3f32c
Amy Tian <Amy Tian>
2016-05-05 16:41:39 -0400

    .

--- upload.php
+++ upload.php
@@ -33,25 +33,25 @@
      include ("readDb.php");
      include ("connectDb.php");
 
-    /* echo '<BR>' .$eName;
+     echo '<BR>' .$eName;
      echo '<BR>' .$startDate;
      echo '<BR>' .$eTime;
      echo '<BR>' .$foodTypes;
      echo '<BR>' .$eLocation;
-     echo '<BR>' .$user;*/
+     echo '<BR>' .$user;
+     echo '<BR> last update - ' .$lastupdate;
 
       // If new user (first time user), update so that they are no longer a new user
      if ($row[isNew] == 1) {
             $sql2 = "UPDATE Members SET isNew=0 WHERE User='$user'";
             $result2 = mysql_query($sql2);
      }
-     $newCount = $row[CurrCount];
 
      // FREE USERS can only have up to 5 events stored in the database
      // If currcount < 5, add new events until it's full
      // If currcount = 5, user is over capacity - don't add anything
       if ($row[isFree] == 1) {
-            if ($newCount < 5) {
+            if ($row[CurrCount] < 5) {
                   // Add to database (depending on whether the event has food or not)
                   if (!$foodTypes) {
                         $sql2 = "INSERT INTO Events (eUser, eventName, startDate, eTime, eLocation, isFood, eDetails) VALUES (('$user'), ('$eName'), ('$startDate'), ('$eTime'), ('$eLocation'), 0, ('$foodTypes'))";
@@ -63,13 +63,17 @@
                   }
 
                   // Update currCount if successful
-                  //if ($result2==1){
-                        $newCount = $row[CurrCount] + 1;
-                        $sql2 = "UPDATE Members SET CurrCount='$newCount' WHERE User='$user'";
+                  if ($result2==1){
+                        $sql2 = "UPDATE Members SET CurrCount=CurrCount+1 WHERE User='$user'";
                         $result2 = mysql_query($sql2);
-                        // $sql_LU = "UPDATE Members SET lastScanDate=('$lastupdate') WHERE User='$user'";
-                        // $result_LU = mysql_query($sql_LU);
-                  //} else die('Invalid query: ' . mysql_error());
+
+
+                        $sql_LU = "UPDATE Members SET lastScanDate=('$lastupdate') WHERE User='$user'";
+                        $result_LU = mysql_query($sql_LU);
+                        if ($result_LU==1){
+                          //do nothing
+                        } else die('invalid query: ' . mysql_error());
+                  } else die('Invalid query: ' . mysql_error());
             }
             else {
                   // Do nothing
@@ -89,11 +93,16 @@
             }
 
             // Update currCount if successful
-            //if ($result2==1){
-                  $newCount = $row[CurrCount] + 1;
-                  $sql2 = "UPDATE Members SET CurrCount='$newCount' WHERE User='$user'";
+            if ($result2==1){
+                  $sql2 = "UPDATE Members SET CurrCount=CurrCount+1 WHERE User='$user'";
                   $result2 = mysql_query($sql2);
-            //} else die('Invalid query: ' . mysql_error());
+
+                        $sql_LU = "UPDATE Members SET lastScanDate=('$lastupdate') WHERE User='$user'";
+                        $result_LU = mysql_query($sql_LU);
+                        if ($result_LU==1){
+                          //do nothing
+                        } else die('invalid query: ' . mysql_error());
+            } else die('Invalid query: ' . mysql_error());
      }
 
       mysql_close($conn);
